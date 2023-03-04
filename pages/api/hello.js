a8a0a2bd8516d047e1771b6adac140395c13bfe6
JULIEN <julien.quennehen@edu.devinci.fr>
2023-03-04 15:58:03 +0100

    test: 💍 serverless node + python

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,20 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+let { PythonShell } = require('python-shell');
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  console.log(req.query);
+
+  let options = {
+    mode: 'text',
+    pythonOptions: ['-u'], // get print results in real-time
+    scriptPath: '../nextjs-serverless/pages/python',
+    args: ['chaco', req.query.value1],
+  };
+
+  PythonShell.run('main.py', options).then((messages) => {
+    // results is an array consisting of messages collected during execution
+    console.log('results: %j', messages);
+    res.status(200);
+    res.setHeader('Content-Type', 'application/json');
+    res.json({ name: messages });
+  });
 }
